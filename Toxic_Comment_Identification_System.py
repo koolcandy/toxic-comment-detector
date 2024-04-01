078152d45430125cf8ae2e7c818d10de6f62f128
koolcandy <133551107+koolcandy@users.noreply.github.com>
2024-04-01 20:47:33 +0800

    change to only use local model

--- Toxic_Comment_Identification_System.py
+++ Toxic_Comment_Identification_System.py
@@ -3,56 +3,24 @@ import os
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
 from utils import train_model
-from utils import check_words
 from utils import get_result_by_local_model
-from utils import get_result_by_gemini
 from utils import config
-import logging
 import urllib.parse
 # encoding:utf-8
 
 def get_result(words):
-    # log_file = os.path.join(config.work_dir, 'log.txt')
-    # logging.basicConfig(filename=log_file, level=logging.INFO)
-    # # Add console logging
 
-    # console = logging.StreamHandler()
-    # console.setLevel(logging.INFO)
+    print('Local bad words not matched, start to use local model to predict...')
+    result = get_result_by_local_model.main(words)
+    print('Local model result: %s' % result)
 
-    # if not logging.getLogger('').hasHandlers():
-    #     print('add console')
-    #     logging.getLogger('').addHandler(console)
-    
-    if check_words.main(words):
-        print('Local bad word matched')
-        result = {
-            'toxic': 1,
-            'severe_toxic': 0,
-            'obscene': 0,
-            'threat': 0,
-            'insult': 0,
-            'identity_hate': 0
-        }
-        return result
-    else:
-        print('Local bad words not matched, start to use local model to predict...')
-        result = get_result_by_local_model.main(words)
-        print('Local model result: %s' % result)
-        toxic_score = result['toxic']
-        if toxic_score > 0.3:
-            return result
-        else:
-            print('Local model not matched, start to use Gemini model to predict...')
-            gemini_result = get_result_by_gemini.main(words)
-            print('Gemini model result: %s' % gemini_result)
-            return gemini_result
+    return result
 
 class RequestHandler(BaseHTTPRequestHandler):
-    def do_POST(self):
-        content_length = int(self.headers['Content-Length'])
-        post_data = self.rfile.read(content_length)
-        post_data = urllib.parse.parse_qs(post_data.decode('utf-8'))
-        text = post_data.get('input', [''])[0]
+    def do_GET(self):
+        query = urllib.parse.urlparse(self.path).query
+        query_components = urllib.parse.parse_qs(query)
+        text = query_components.get('input', [''])[0]
         result = get_result(text)
         self.send_response(200)
         self.send_header('Content-type', 'application/json')
